3ffb7ae093ed6d486883aea61510e8ec22f79118
Your Name <your.email@example.com>
2025-05-04 19:59:01 +0200

    espace pedarogique pour student

--- gestion_institut/src/pages/CoursProf.jsx
+++ gestion_institut/src/pages/CoursProf.jsx
@@ -1,103 +1,62 @@
 import React, { useState } from 'react';
+import { Container, Typography, TextField, Box, Paper, InputLabel} from '@mui/material';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+
+const MyBotton = styled(Button)({
+  background: 'linear-gradient(45deg, #1E3A8A 30%, #3B82F6 90%)',
+  border: 0,
+  borderRadius: 3,
+  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  color: 'white',
+  height: 48,
+  padding: '0 30px',
+  marginTop: 16,
+});
 
 const CoursProf = () =>{
   const [titre, setTitre] = useState('');
   const [fichier, setFichier] = useState(null);
-
   const handleSubmit = (e) =>{
     e.preventDefault();
     console.log('Titre :', titre);
     console.log('Fichier :',fichier);
     alert('Cours uploade avec succes !');
   };
-  return(
-    <div style={styles.container}>
-      <div style={styles.formContainer}>
-      <h2>Uploader un cours</h2>
-      <form onSubmit={handleSubmit}>
-      <label>Titre du Cours : </label><br />
-      <input 
-          type="text"
+  
+  return( 
+    <Container maxWidth="sm" sx={{ mt: 6}}>
+      <Paper elevattion={3} sx= {{ p:4, borderRadius: 3}}>
+        <Typography variant="h5" gutterBottom align="center">
+          Uploader un Cours
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate>
+          <TextField
+          label="Titre du cours"
+          variant="outlined"
+          fullWidth
+          required
           value={titre}
           onChange={(e) => setTitre(e.target.value)}
-          required
-          style={{ padding: 8, marginBottom: 10, width: '100%'}}
+          sx={{ mb:3}}
           />
-          <label>Fichier PDF :</label>
+
+          <InputLabel sx= {{ mb: 1}}>Fichier (PDF)</InputLabel>
           <input
           type="file"
           accept=".pdf"
-          onChange={(e) => setFichier(e.target.files[0])}
+          onCharge={(e) => setFichier(e.target.files[0])}
           required
-          style={{ }}
+          style={{ marginBottom: 20}}
           />
-          <button type="submit" style={styles.button}>Uploader</button>
 
-      </form>
-
-      
-        </div>
-                
-        </div>
+          <MyBotton type="submit" fullWidth>
+            Uploader
+          </MyBotton>
+        </Box>
+      </Paper>
+    </Container>
   );
-};
-
-
-
-const styles = {
-  container: {
-    minHeight: '100vh',
-    display: 'flex',
-    justifyContent: 'flex',
-    alignItems: 'center',
-    background: '#f9f9f9',
-    padding: 20,
-  },
-  formContainer: {
-    background: '#fff',
-    padding: 30,
-    borderRadius: 12,
-    boxShadow: '0 6px 10px rgba(0, 0, 0, 0.1)',
-    width: '100%',
-    maxWidth: 400,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
-    color: '#007BFF',
-    textAlign: 'center',
-  },
-  label: {
-    display: 'block',
-    marginBottom: 6,
-    fontWeight: 500,
-    color: '#333',
-  },
-  input: {
-    width: '100%',
-    padding: '10px 12px',
-    marginBottom: 15,
-    border: '1px solid #ccc',
-    borderRadius: 8,
-    fontSize: 16,
-  },
-  fileInput: {
-    width: '100%',
-    marginBottom: 20,
-  },
-  button: {
-    width: '100%',
-    padding: 12,
-    background: '#007BFF',
-    color: '#fff',
-    border: 'none',
-    borderRadius: 8,
-    fontSize: 16,
-    fontWeight: 'bold',
-    cursor: 'pointer',
-    transition: 'background 0.3s',
-  },
 };
   
   export default CoursProf;
